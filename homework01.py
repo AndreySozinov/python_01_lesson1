a44278fb556628015a99d671b8adf0bc797a5191
AndreySozinov <savrey@ya.ru>
2023-03-08 19:50:34 +0500

    Task 1 from homework done

--- homework01.py
+++ homework01.py
@@ -6,3 +6,15 @@
 # то треугольника с такими сторонами не существует.
 # Отдельно сообщить является ли треугольник разносторонним, равнобедренным или равносторонним.
 
+first_side = int(input("Длина первой стороны: "))
+second_side = int(input("Длина второй стороны: "))
+third_side = int(input("Длина третьей стороны: "))
+
+if first_side >= second_side + third_side or second_side >= first_side + third_side or third_side >= first_side + second_side:
+    print("Такого треугольника не существует.")
+elif first_side == second_side and first_side == third_side:
+    print("Треугольник равносторонний.")
+elif first_side == second_side or first_side == third_side or second_side == third_side:
+    print("Треугольник равнобедренный.")
+else:
+    print("Треугольник разносторонний.")
